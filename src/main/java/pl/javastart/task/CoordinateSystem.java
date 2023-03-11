6d369eb7b017ca83c352192bc991f459aaf79241
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-03-11 13:49:17 +0100

    Rozwiązanie

--- src/main/java/pl/javastart/task/CoordinateSystem.java
+++ src/main/java/pl/javastart/task/CoordinateSystem.java
@@ -6,6 +6,29 @@ public class CoordinateSystem {
 
     // uzupełnij metodę. Używaj scannera przekazanego w parametrze.
     void run(Scanner scanner) {
-        System.out.println("Punkt (5, -3)");
+        Point point = new Point();
+        System.out.println("Podaj X");
+        point.setX(scanner.nextInt());
+        System.out.println("Podaj Y");
+        point.setY(scanner.nextInt());
+        location(point.getX(), point.getY());
+    }
+
+    private void location(int x, int y) {
+        if (x == 0 && y == 0) {
+            System.out.printf("Punkt (%d, %d) leży na środku układu współrzędnych", x, y);
+        } else if (x == 0) {
+            System.out.printf("Punkt (%d, %d) leży na osi Y", x, y);
+        } else if (y == 0) {
+            System.out.printf("Punkt (%d, %d) leży na osi X", x, y);
+        } else if (x > 0 && y > 0) {
+            System.out.printf("Punkt (%d, %d) leży w I ćwiartce układu współrzędnych", x, y);
+        } else if (x < 0 && y > 0) {
+            System.out.printf("Punkt (%d, %d) leży w II ćwiartce układu współrzędnych", x, y);
+        } else if (x < 0) {
+            System.out.printf("Punkt (%d, %d) leży w III ćwiartce układu współrzędnych", x, y);
+        } else {
+            System.out.printf("Punkt (%d, %d) leży w IV ćwiartce układu współrzędnych", x, y);
+        }
     }
 }
